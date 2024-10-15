16b3bacbcf2a9e967fa2a988b6954325453b7daa
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2024-10-15 14:54:29 -0300

    formatted functions.py for better readability. changed embedding model to newer, cheaper one

--- app/functions.py
+++ app/functions.py
@@ -1,21 +1,21 @@
-
-from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_community.vectorstores import Chroma
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.prompts import ChatPromptTemplate
-from pydantic import BaseModel, Field
-
 import os
+import re
 import tempfile
 import uuid
+
 import pandas as pd
-import re
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Chroma
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from pydantic import BaseModel, Field
+
 
 def clean_filename(filename):
     """
-    Remove "(number)" pattern from a filename 
+    Remove "(number)" pattern from a filename
     (because this could cause error when used as collection name when creating Chroma database).
 
     Parameters:
@@ -25,10 +25,11 @@ def clean_filename(filename):
         str: The cleaned filename
     """
     # Regular expression to find "(number)" pattern
-    new_filename = re.sub(r'\s\(\d+\)', '', filename)
+    new_filename = re.sub(r"\s\(\d+\)", "", filename)
     return new_filename
 
-def get_pdf_text(uploaded_file): 
+
+def get_pdf_text(uploaded_file):
     """
     Load a PDF document from an uploaded file and return it as a list of documents
 
@@ -53,13 +54,13 @@ def get_pdf_text(uploaded_file):
         documents = loader.load()
 
         return documents
-    
+
     finally:
         # Ensure the temporary file is deleted when we're done with it
         os.unlink(temp_file.name)
 
 
-def split_document(documents, chunk_size, chunk_overlap):    
+def split_document(documents, chunk_size, chunk_overlap):
     """
     Function to split generic text into smaller chunks.
     chunk_size: The desired maximum size of each chunk (default: 400)
@@ -68,11 +69,13 @@ def split_document(documents, chunk_size, chunk_overlap):
     Returns:
         list: A list of smaller text chunks created from the generic text
     """
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size,
-                                          chunk_overlap=chunk_overlap,
-                                          length_function=len,
-                                          separators=["\n\n", "\n", " "])
-    
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=len,
+        separators=["\n\n", "\n", " "],
+    )
+
     return text_splitter.split_documents(documents)
 
 
@@ -89,13 +92,12 @@ def get_embedding_function(api_key):
         OpenAIEmbeddings: An OpenAIEmbeddings object, which can be used to create vector embeddings from text.
     """
     embeddings = OpenAIEmbeddings(
-        model="text-embedding-ada-002", openai_api_key=api_key
+        model="text-embedding-3-small", openai_api_key=api_key
     )
     return embeddings
 
 
 def create_vectorstore(chunks, embedding_function, file_name, vector_store_path="db"):
-
     """
     Create a vector store from a list of text chunks.
 
@@ -109,34 +111,35 @@ def create_vectorstore(chunks, embedding_function, file_name, vector_store_path=
 
     # Create a list of unique ids for each document based on the content
     ids = [str(uuid.uuid5(uuid.NAMESPACE_DNS, doc.page_content)) for doc in chunks]
-    
+
     # Ensure that only unique docs with unique ids are kept
     unique_ids = set()
     unique_chunks = []
-    
-    unique_chunks = [] 
-    for chunk, id in zip(chunks, ids):     
-        if id not in unique_ids:       
+
+    unique_chunks = []
+    for chunk, id in zip(chunks, ids):
+        if id not in unique_ids:
             unique_ids.add(id)
-            unique_chunks.append(chunk)        
+            unique_chunks.append(chunk)
 
     # Create a new Chroma database from the documents
-    vectorstore = Chroma.from_documents(documents=unique_chunks, 
-                                        collection_name=clean_filename(file_name),
-                                        embedding=embedding_function, 
-                                        ids=list(unique_ids), 
-                                        persist_directory = vector_store_path)
+    vectorstore = Chroma.from_documents(
+        documents=unique_chunks,
+        collection_name=clean_filename(file_name),
+        embedding=embedding_function,
+        ids=list(unique_ids),
+        persist_directory=vector_store_path,
+    )
 
     # The database should save automatically after we create it
     # but we can also force it to save using the persist() method
     vectorstore.persist()
-    
+
     return vectorstore
 
 
 def create_vectorstore_from_texts(documents, api_key, file_name):
-    
-    # Step 2 split the documents  
+    # Step 2 split the documents
     """
     Create a vector store from a list of texts.
 
@@ -147,31 +150,33 @@ def create_vectorstore_from_texts(documents, api_key, file_name):
     :return: A Chroma vector store object
     """
     docs = split_document(documents, chunk_size=1000, chunk_overlap=200)
-    
+
     # Step 3 define embedding function
     embedding_function = get_embedding_function(api_key)
 
-    # Step 4 create a vector store  
+    # Step 4 create a vector store
     vectorstore = create_vectorstore(docs, embedding_function, file_name)
-    
+
     return vectorstore
 
 
 def load_vectorstore(file_name, api_key, vectorstore_path="db"):
-
     """
     Load a previously saved Chroma vector store from disk.
 
     :param file_name: The name of the file to load (without the path)
     :param api_key: The OpenAI API key used to create the vector store
     :param vectorstore_path: The path to the directory where the vector store was saved (default: "db")
-    
+
     :return: A Chroma vector store object
     """
     embedding_function = get_embedding_function(api_key)
-    return Chroma(persist_directory=vectorstore_path, 
-                  embedding_function=embedding_function, 
-                  collection_name=clean_filename(file_name))
+    return Chroma(
+        persist_directory=vectorstore_path,
+        embedding_function=embedding_function,
+        collection_name=clean_filename(file_name),
+    )
+
 
 # Prompt template
 PROMPT_TEMPLATE = """
@@ -187,19 +192,27 @@ don't know. DON'T MAKE UP ANYTHING.
 Answer the question based on the above context: {question}
 """
 
+
 class AnswerWithSources(BaseModel):
     """An answer to the question, with sources and reasoning."""
+
     answer: str = Field(description="Answer to question")
-    sources: str = Field(description="Full direct text chunk from the context used to answer the question")
-    reasoning: str = Field(description="Explain the reasoning of the answer based on the sources")
-    
+    sources: str = Field(
+        description="Full direct text chunk from the context used to answer the question"
+    )
+    reasoning: str = Field(
+        description="Explain the reasoning of the answer based on the sources"
+    )
+
 
 class ExtractedInfoWithSources(BaseModel):
     """Extracted information about the research article"""
+
     paper_title: AnswerWithSources
     paper_summary: AnswerWithSources
     publication_year: AnswerWithSources
     paper_authors: AnswerWithSources
+
 
 def format_docs(docs):
     """
@@ -211,10 +224,10 @@ def format_docs(docs):
     """
     return "\n\n".join(doc.page_content for doc in docs)
 
+
 # retriever | format_docs passes the question through the retriever, generating Document objects, and then to format_docs to generate strings;
 # RunnablePassthrough() passes through the input question unchanged.
 def query_document(vectorstore, query, api_key):
-
     """
     Query a vector store with a question and return a structured response.
 
@@ -226,15 +239,17 @@ def query_document(vectorstore, query, api_key):
     """
     llm = ChatOpenAI(model="gpt-4o-mini", api_key=api_key)
 
-    retriever=vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
+    retriever = vectorstore.as_retriever(
+        search_type="similarity", search_kwargs={"k": 6}
+    )
 
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
 
     rag_chain = (
-            {"context": retriever | format_docs, "question": RunnablePassthrough()}
-            | prompt_template
-            | llm.with_structured_output(ExtractedInfoWithSources, strict=True)
-        )
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt_template
+        | llm.with_structured_output(ExtractedInfoWithSources, strict=True)
+    )
 
     structured_response = rag_chain.invoke(query)
     df = pd.DataFrame([structured_response.dict()])
@@ -245,11 +260,15 @@ def query_document(vectorstore, query, api_key):
     reasoning_row = []
 
     for col in df.columns:
-        answer_row.append(df[col][0]['answer'])
-        source_row.append(df[col][0]['sources'])
-        reasoning_row.append(df[col][0]['reasoning'])
+        answer_row.append(df[col][0]["answer"])
+        source_row.append(df[col][0]["sources"])
+        reasoning_row.append(df[col][0]["reasoning"])
 
     # Create new dataframe with two rows: 'answer' and 'source'
-    structured_response_df = pd.DataFrame([answer_row, source_row, reasoning_row], columns=df.columns, index=['answer', 'source', 'reasoning'])
-  
+    structured_response_df = pd.DataFrame(
+        [answer_row, source_row, reasoning_row],
+        columns=df.columns,
+        index=["answer", "source", "reasoning"],
+    )
+
     return structured_response_df.T
