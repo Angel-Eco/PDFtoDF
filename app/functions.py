6ab7dbf2d2ecfd99611947c285b5d14bb1b172ad
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2024-10-15 14:56:00 -0300

    deleted deprecated chromadb .from_documents parameter

--- app/functions.py
+++ app/functions.py
@@ -128,7 +128,6 @@ def create_vectorstore(chunks, embedding_function, file_name, vector_store_path=
         collection_name=clean_filename(file_name),
         embedding=embedding_function,
         ids=list(unique_ids),
-        persist_directory=vector_store_path,
     )
 
     # The database should save automatically after we create it
